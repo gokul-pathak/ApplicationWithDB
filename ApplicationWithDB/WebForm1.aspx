67f36b5294c77002e7d2a81115f4d08a6898e689
Gokul Pathak <gokul_pathak@yahoo.com>
2023-06-19 09:15:23 +0545

    Implement form submission and database insertion functionality

---
+++ ApplicationWithDB/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ApplicationWithDB.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+    <script src="Scripts/bootstrap.min.js"></script>
+</head>
+<body>
+<form id="form1" runat="server">
+    <div class="container mt-5">
+  <div class="mb-3">
+    <label for="idInput" class="form-label">ID</label>
+    <input type="text" class="form-control" id="idInput" runat="server" name="idInput"/>
+  </div>
+  <div class="mb-3">
+    <label for="nameInput" class="form-label">Name</label>
+    <input type="text" class="form-control" runat="server" id="nameInput" name="nameInput"/>
+  </div>
+  <asp:button type="submit" runat="server" class="btn btn-primary" onclick="SubmitButton_Click"></asp:button>
+        </div>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" ProviderName="<%$ ConnectionStrings:ConnectionString.ProviderName %>" SelectCommand="SELECT * FROM [Table]"></asp:SqlDataSource>
+    <asp:SqlDataSource ID="SqlDataSource2" runat="server"></asp:SqlDataSource>
+</form>
+
+
+</body>
+</html>
---
+++ ApplicationWithDB/WebForm1.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace ApplicationWithDB
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Code to be executed on page load (if needed)
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            // Retrieve the value from the input field
+            string Name = this.nameInput.Value;
+            try
+            {
+                // Database connection string
+                string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+                // SQL query to insert data into the table
+                string sqlQuery = "INSERT INTO [dbo].[Table] ([Name]) VALUES (@Name)";
+
+                
+
+                // Create a SqlConnection object
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // Create a SqlCommand object with the query and connection
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        // Add parameter to the command
+                        command.Parameters.AddWithValue("@Name", Name);
+
+                        // Open the connection
+                        connection.Open();
+
+                        // Execute the command
+                        command.ExecuteNonQuery();
+                        Response.Write("Student name registered");  
+                        // Close the connection
+                        connection.Close();
+                    }
+                }
+
+                // Optionally, perform any additional actions or display success message
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during database operations
+                // Optionally, display error message or perform error handling
+                Response.Write("Error" + ex.ToString());
+            }
+        }
+    }
+}
