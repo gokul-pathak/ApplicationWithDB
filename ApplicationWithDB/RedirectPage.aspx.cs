c68ed15a7691812b383e6e183a2500cec345e7e7
Gokul Pathak <gokul_pathak@yahoo.com>
2023-06-26 08:11:39 +0545

    page redirected and sessions related practice

---
+++ ApplicationWithDB/RedirectPage.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ApplicationWithDB
+{
+    public partial class RedirectPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //QueryString
+            Label1.Text = "Welcome" + Request.QueryString["myname"];
+            //Session
+            if (Session["username"] == null)
+            {
+                Response.Redirect("ViewState.aspx");
+            }
+            else
+            {
+                LoginRedirect.Text = Session["username"].ToString();
+            }
+
+        }
+        protected void Logout_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ViewState.aspx");
+        }
+    }
+}
---
+++ ApplicationWithDB/ViewState.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ApplicationWithDB
+{
+    public partial class ViewState : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            ViewState["myValue"] = TextBox1.Text.ToString();
+            //myValue = TextBox1.Text.ToString();
+            TextBox1.Text = string.Empty;
+        }
+        protected void restore_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = ViewState["myValue"].ToString();
+        }
+        protected void submitQuery_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("RedirectPage.aspx?myname=" + TextBox2.Text.ToString());
+        }
+        protected void LoginBtn_Click(object sender, EventArgs e)
+        {
+            if (UserName.Text.ToString() == "ram")
+            {
+                //create session
+                Session["username"] = UserName.Text.ToString();
+                Response.Redirect("RedirectPage.aspx");
+            }
+        }
+    }
+}
